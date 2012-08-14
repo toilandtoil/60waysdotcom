635d8850ae8ac66e7349edb43bf13c4d6de9a423
Michael Buckley <mbuckl1@60ways.com>
2012-08-14 16:55:56 -0500

    Trying to fix the URL onclick event.

--- index.php
+++ index.php
@@ -52,7 +52,7 @@ Replace UA-XXXXXX-XX with your site's ID and uncomment to enable.-->
         </ul>
         <div class="footer">
             <a href="#" id="prev" onClick="slider60ways.prev();return false;">prev</a>
-            <a href="#" id="copyright" onClick="http://60ways.com/buy">® 2012 Michael Buckley & Toil and Toil Books</a>
+            <a href="http://www.60ways.com/buy" id="copyright">® 2012 Michael Buckley & Toil and Toil Books</a>
             <a href="#" id="next" onClick="slider60ways.next();return false;">next</a>
         </div>
     </div>
