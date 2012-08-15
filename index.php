328f5ee6a7f0f20063942271ccde6b70babc54d1
Michael Buckley <michael@toilandtoil.com>
2012-08-14 21:52:09 -0500

    Moving google analytics out of index.php; moving footer out of index.php;

--- index.php
+++ index.php
@@ -9,13 +9,12 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <script type="text/javascript">
         document.cookie = 'resolution=' + Math.max(screen.width, screen.height) + ("devicePixelRatio" in window ? "," + devicePixelRatio : ",1") + '; path=/';
-
     </script>
-    <!-- Asynchronous google analytics; this is the official snippet.
-Replace UA-XXXXXX-XX with your site's ID and uncomment to enable.-->
-
+    <script src="js/jquery.js" type="text/javascript"></script>
+    <script src="js/jquery.lazyload.js" type="text/javascript"></script>
+    <!--<script src="js/jquery.scrollstop.js" type="text/javascript"></script>-->
+    <!--  Asynchronous google analytics; this is the official snippet. Replace UA-XXXXXX-XX with your site's ID and uncomment to enable.
     <script>
-
         var _gaq = _gaq || [];
         _gaq.push(['_setAccount', 'UA-3340666-2']);
         _gaq.push(['_trackPageview']);
@@ -28,12 +27,10 @@ Replace UA-XXXXXX-XX with your site's ID and uncomment to enable.-->
             var s = document.getElementsByTagName('script')[0];
             s.parentNode.insertBefore(ga, s);
         })();
-    </script>
-    <script>//some script here. Just testing.</script>
-    <script src="js/jquery.js" type="text/javascript"></script>
-    <script src="js/jquery.lazyload.js" type="text/javascript"></script>
-    <script src="js/jquery.scrollstop.js" type="text/javascript"></script>
+    </script> -->
+
     <script src='js/swipe.js' type="text/javascript"></script>
+    <script src='js/google_analytics.js' type="text/javascript"></script>
     <link rel="stylesheet" href="./css/base.css" type="text/css" media="all">
     <link rel="stylesheet" href="./css/narrow.css" type="text/css"
           media="screen and (max-height: 485px) and (orientation:portrait)">
@@ -44,46 +41,23 @@ Replace UA-XXXXXX-XX with your site's ID and uncomment to enable.-->
 </head>
 <body>
 <?php include 'php/60ways_book_slideshow.php'; ?>
+
 <div class="wrapper">
     <div id="slider60ways">
         <ul>
             <?=$products
             ?>
         </ul>
-        <div class="footer">
-            <a href="#" id="prev" onClick="slider60ways.prev();return false;">prev</a>
-            <a href="http://www.60ways.com/buy" id="copyright">® 2012 Michael Buckley & Toil and Toil Books</a>
-            <a href="#" id="next" onClick="slider60ways.next();return false;">next</a>
-        </div>
     </div>
 </div>
+
+<?php include 'php/footer.php'; ?>
 </body>
 <script type="text/javascript">
     //http://stackoverflow.com/questions/2517408/how-can-i-implement-lazy-loading-on-a-page-with-500-images
     //https://docs.google.com/spreadsheet/ccc?key=0Aqln2akPWiMIdERkY3J2OXdOUVJDTkNSQ2ZsV3hoWVE#gid=2
     //http://net.tutsplus.com/tutorials/javascript-ajax/how-to-create-an-infinite-scroll-web-gallery/
     //http://stackoverflow.com/questions/6173382/cant-able-to-get-the-properties-of-touchstart-event
-    //make a function that will set the src attribute to the "next" image
-    function loadLazy() {
-        $slideNumber = slider60ways.getPos();
-        $slideImageContainer = $('#slideImage60ways' + $slideNumber);
-        $prevSlideImageContainer = $('#slideImage60ways' + ($slideNumber - 1));
-        $nextSlideImageContainer = $('#slideImage60ways' + ($slideNumber + 1));
-        $nextNextSlideImageContainer = $('#slideImage60ways' + ($slideNumber + 2));
-        //alert($slideImageContainer.selector);
-        // retrieve the image inside this element
-        var elem = $slideImageContainer.find('img');
-        var prevElem = $prevSlideImageContainer.find('img');
-        var nextElem = $nextSlideImageContainer.find('img');
-        var nextNextElem = $nextNextSlideImageContainer.find('img');
-        // set the 'src' to the 'data-original' value
-        elem.attr('src', elem.data('original'));
-        prevElem.attr('src', prevElem.data('original'));
-        nextElem.attr('src', nextElem.data('original'));
-        nextNextElem.attr('src', nextNextElem.data('original'));
-    }
-
-    //reference:
     //http://stackoverflow.com/questions/10281210/jquery-update-image-src-on-hover
     //http://stackoverflow.com/questions/652763/jquery-object-to-string
     //http://www.neowin.net/forum/topic/696738-passing-a-variable-to-a-jquery-selector/
